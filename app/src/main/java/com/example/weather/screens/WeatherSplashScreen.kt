5d4f51d103ea4ceef7c59d262f2395ab0a0a0d19
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-18 20:45:35 +0530

    Create Splash Screen UI

--- app/src/main/java/com/example/weather/screens/WeatherSplashScreen.kt
+++ app/src/main/java/com/example/weather/screens/WeatherSplashScreen.kt
@@ -1,10 +1,50 @@
 package com.example.weather.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.weather.R
 
 @Composable
-fun WeatherSplashScreen(navController: NavController){
-Text(text = "Splash Screen")
+fun WeatherSplashScreen(navController: NavController) {
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(
+            width = 2.dp,
+            color = Color.LightGray
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sun),
+                contentDescription = "Sun Image",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(100.dp)
+            )
+            Text(text = "Find the Sun?", fontSize = 20.sp, color = Color.LightGray)
+        }
+    }
 }
