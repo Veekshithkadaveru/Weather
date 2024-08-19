dfa82fbf9886c62abaaa0e567352b7a21bbd31a7
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-19 22:16:20 +0530

    Create Weather App Module

--- app/src/main/java/com/example/weather/di/AppModule.kt
+++ app/src/main/java/com/example/weather/di/AppModule.kt
@@ -1,4 +1,26 @@
 package com.example.weather.di
 
+import com.example.weather.network.WeatherApi
+import com.example.weather.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModule {
+    @Provides
+    @Singleton
+    fun provideOpenWeatherApi(): WeatherApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create()
+    }
 }
