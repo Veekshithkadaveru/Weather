635a99f31c115b434f1ae1d56672a6ab84251228
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-26 19:38:22 +0530

    Refactor Code

---
+++ app/src/main/java/com/example/weather/widgets/Widgets.kt
@@ -0,0 +1,180 @@
+package com.example.weather.widgets
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.weather.R
+import com.example.weather.model.WeatherItem
+import com.example.weather.util.formatDate
+import com.example.weather.util.formatDateTime
+import com.example.weather.util.formatDecimals
+
+
+@Composable
+fun WeatherDetailRow(weather: WeatherItem) {
+    val imageUrl = "https://api.openweathermap.org/img/wn/${weather.weather[0].icon}.png"
+    Surface(
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth(),
+        shape = CircleShape.copy(topEnd = CornerSize(6.dp))
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                formatDate(weather.dt).split(",")[0],
+                modifier = Modifier.padding(start = 5.dp)
+            )
+            WeatherStateImage(imageUrl = imageUrl)
+            Surface(
+                modifier = Modifier.padding(0.dp),
+                shape = CircleShape,
+                color = Color(0xFFFFC400)
+            ) {
+                Text(
+                    weather.weather[0].description,
+                    modifier = Modifier.padding(4.dp),
+                    style = MaterialTheme.typography.labelSmall
+                )
+            }
+            Text(text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Blue.copy(alpha = 0.7f),
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append(formatDecimals(weather.temp.max) + "º")
+                }
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.LightGray
+                    )
+                ) {
+                    append(formatDecimals(weather.temp.min) + "º")
+                }
+            })
+        }
+    }
+
+}
+
+@Composable
+fun SunsetandriseRow(weather: WeatherItem) {
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.sunrise),
+                contentDescription = "Sunrise icon",
+                modifier = Modifier.size(30.dp)
+            )
+            Text(
+                text = formatDateTime(weather.sunrise),
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.sunset),
+                contentDescription = "Sunset icon",
+                modifier = Modifier.size(30.dp)
+            )
+            Text(
+                text = formatDateTime(weather.sunset),
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+    }
+
+}
+
+@Composable
+fun HumidityWindPressureRow(weather: WeatherItem) {
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.humidity),
+                contentDescription = "Humidity icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather.humidity}%",
+                style = MaterialTheme.typography.bodySmall
+            )
+
+        }
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.pressure),
+                contentDescription = "Pressure icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather.pressure}%",
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+        Row {
+            Icon(
+                painter = painterResource(id = R.drawable.wind),
+                contentDescription = "Wind icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather.humidity} mph",
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+    }
+}
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .crossfade(true)
+            .build(),
+        contentDescription = "icon",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.size(60.dp)
+    )
+}
+
+
