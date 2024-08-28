c6ba237ec6915a04e813d42797011c8006bbc1ed
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-28 19:21:59 +0530

    Create DropDownMenu

--- app/src/main/java/com/example/weather/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/example/weather/widgets/WeatherAppBar.kt
@@ -1,16 +1,34 @@
 package com.example.weather.widgets
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -31,6 +49,15 @@ fun WeatherAppBar(
     onAddActionClicked: () -> Unit = {},
     onButtonClicked: () -> Unit = {}
 ) {
+    val showDialog = remember {
+        mutableStateOf(false)
+    }
+    if (showDialog.value) {
+        SettingDropDownMenu(
+            showDialog = showDialog,
+            navController = navController
+        )
+    }
     TopAppBar(
         title = {
             Text(
@@ -44,13 +71,15 @@ fun WeatherAppBar(
         },
         actions = {
             if (isMainScreen) {
-                IconButton(onClick = { onAddActionClicked.invoke()}) {
+                IconButton(onClick = { onAddActionClicked.invoke() }) {
                     Icon(
                         imageVector = Icons.Default.Search,
                         contentDescription = "search icon"
                     )
                 }
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = {
+                    showDialog.value = true
+                }) {
                     Icon(
                         imageVector = Icons.Filled.MoreVert,
                         contentDescription = "Vertical icon"
@@ -77,3 +106,54 @@ fun WeatherAppBar(
         elevation = elevation
     )
 }
+
+@Composable
+fun SettingDropDownMenu(
+    showDialog: MutableState<Boolean>,
+    navController: NavController
+) {
+    var expanded by remember {
+        mutableStateOf(true)
+    }
+    val items = listOf("About", "Favourites", "Settings")
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopEnd)
+            .absolutePadding(top = 45.dp, right = 20.dp)
+    ) {
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(140.dp)
+                .background(Color.White)
+        ) {
+            items.forEachIndexed { index, text ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    showDialog.value = false
+                }) {
+                    Row {
+                        Icon(
+                            imageVector = when (text) {
+                                "About" -> Icons.Default.Info
+                                "Favourites" -> Icons.Default.FavoriteBorder
+                                else -> Icons.Default.Settings
+                            },
+                            contentDescription = null,
+                            tint = Color.LightGray
+                        )
+                    }
+                    Text(
+                        text = text,
+                        modifier = Modifier.clickable { },
+                        fontWeight = FontWeight.W300
+                    )
+                }
+            }
+        }
+    }
+}
+
+
