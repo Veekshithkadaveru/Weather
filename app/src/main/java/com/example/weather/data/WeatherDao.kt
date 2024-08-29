c69043914f98f63c749df09a9f5f17197daa638c
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-29 15:17:11 +0530

    Create Favourite Dao

---
+++ app/src/main/java/com/example/weather/data/WeatherDao.kt
@@ -0,0 +1,31 @@
+package com.example.weather.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.weather.model.Favorite
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WeatherDao {
+    @Query("SELECT * from fav_table")
+    fun getFavorites(): Flow<List<Favorite>>
+
+    @Query("SELECT * from fav_table WHERE city=:city")
+    suspend fun getFavouriteById(city: String): Favorite
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavourite(favorite: Favorite)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateFavourite(favorite: Favorite)
+
+    @Query("DELETE from fav_table")
+    suspend fun deleteAllFavourites()
+
+    @Delete
+    suspend fun deleteFavorites(favorite: Favorite)
+}
