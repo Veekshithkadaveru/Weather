153059897220f651bd4ed1c275d4865e7f1c9331
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-30 18:06:20 +0530

    Create Weather Database

---
+++ app/src/main/java/com/example/weather/data/WeatherDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.weather.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.weather.model.Favorite
+
+@Database(entities = [Favorite::class], version = 1, exportSchema = false)
+abstract class WeatherDatabase : RoomDatabase() {
+    abstract fun weatherDao(): WeatherDao
+}
