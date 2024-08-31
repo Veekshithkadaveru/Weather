539f89a80754cf581b3dfc3acece3a62c4461088
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-31 22:00:06 +0530

    Create Settings Screen UI

--- app/src/main/java/com/example/weather/screens/settings/Settings.kt
+++ app/src/main/java/com/example/weather/screens/settings/Settings.kt
@@ -1,10 +1,81 @@
 package com.example.weather.screens.settings
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.weather.widgets.WeatherAppBar
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun SettingsScreen(navController: NavController){
-    Text(text = "Settings Screen")
+fun SettingsScreen(navController: NavController) {
+    var unitToggleState by remember { mutableStateOf(false) }
+    val measurementsUnits = listOf("Imperial(F)", "Metric(C)")
+    var choiceState by remember { mutableStateOf("") }
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = "Settings",
+            icon = Icons.AutoMirrored.Default.ArrowBack,
+            isMainScreen = false,
+            navController = navController
+        )
+    }
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Change Units of Measurement",
+                    modifier = Modifier.padding(15.dp)
+                )
+                IconToggleButton(
+                    checked = !unitToggleState,
+                    onCheckedChange = {
+                        unitToggleState = !it
+                        if (unitToggleState) {
+                            choiceState = "Imperial(F)"
+                        } else {
+                            choiceState = "Metric(C)"
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .clip(shape = RectangleShape)
+                        .padding(5.dp)
+                        .background(
+                            Color.Magenta.copy(alpha = 0.4f)
+                        )
+                ) {
+                    Text(text = if (unitToggleState) "Fahrenheit °F" else "Celsius °C")
+                }
+            }
+        }
+    }
 }
